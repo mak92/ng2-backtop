f4e5c787a24f87b6fee6b25462f49db57cdde068
Ioannis Makris <ioannis@makris.com>
2017-04-04 14:40:52 +0300

    Simplified scroll show method

--- src/app/shared/ng2-backtop.directive.ts
+++ src/app/shared/ng2-backtop.directive.ts
@@ -1,8 +1,8 @@
-import {Component, Directive, Input, Output, ElementRef, EventEmitter, HostListener, OnInit} from '@angular/core';
+import {Component, Input, ElementRef, HostListener} from '@angular/core';
 
 
 @Component({
-  selector: 'backtop', 
+  selector: 'backtop',
   template: '<div id="backtop" class="{{theme}} {{class}}"><button><i class="fa fa-chevron-up"></i></button></div>',
    styleUrls: ['ng2-backtop.css']
 
@@ -10,61 +10,68 @@ import {Component, Directive, Input, Output, ElementRef, EventEmitter, HostListe
 export class BacktopDirective {
   @Input('scrollspeed') speed;
   @Input('buttontheme') theme;
-  class:string;
+  class: string;
 constructor(private el: ElementRef) {
-    this.el = el
-    var self = this;
+    this.el = el;
+    const self = this;
     window.addEventListener('scroll', function () {
-                if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-                    self.class= "show";
+                if (window.scrollY >= 200) {
+                    self.class = 'show';
                 } else {
-                    self.class= "";
+                    self.class = '';
                 }
             });
 }
 
 
 @HostListener('click', ['$event.target']) onClick(btn) {
-    this.smoothScroll()
-    this.class="";
+    this.smoothScroll();
+    this.class = '';
   }
 
 
 
 currentYPosition() {
-                if (self.pageYOffset)
-                    return self.pageYOffset;
-                if (document.documentElement && document.documentElement.scrollTop)
-                    return document.documentElement.scrollTop;
-                if (document.body.scrollTop)
-                    return document.body.scrollTop;
+                if (self.pageYOffset) {
+                  return self.pageYOffset;
+                }
+                if (document.documentElement && document.documentElement.scrollTop) {
+                  return document.documentElement.scrollTop;
+                }
+                if (document.body.scrollTop) {
+                  return document.body.scrollTop;
+                }
                 return 0;
             };
  smoothScroll() {
-                var startY = this.currentYPosition();
-                var stopY = 0;
-                var distance = stopY > startY ? stopY - startY : startY - stopY;
+                const startY = this.currentYPosition();
+                const stopY = 0;
+                const distance = stopY > startY ? stopY - startY : startY - stopY;
                 if (distance < 100) {
                     scrollTo(0, stopY);
                     return;
                 }
-                var speed = this.speed  ? Math.round(this.speed / 100) : 6 ;
-                var step = Math.round(distance / 25);
-                var leapY = stopY > startY ? startY + step : startY - step;
-                var timer = 0;
+                const speed = this.speed  ? Math.round(this.speed / 100) : 6 ;
+                const step = Math.round(distance / 25);
+                let leapY = stopY > startY ? startY + step : startY - step;
+                let timer = 0;
                 if (stopY > startY) {
-                    for (var i = startY; i < stopY; i += step) {
+                    for (let i = startY; i < stopY; i += step) {
                         setTimeout("window.scrollTo(0, " + leapY + ")", timer * speed);
                         leapY += step;
-                        if (leapY > stopY) leapY = stopY;
+                        if (leapY > stopY) {
+                          leapY = stopY;
+                        }
                         timer++;
                     }
                     return;
                 }
-                for (var j = startY; j > stopY; j -= step) {
+                for (let j = startY; j > stopY; j -= step) {
                     setTimeout("window.scrollTo(0, " + leapY + ")", timer * speed);
                     leapY -= step;
-                    if (leapY < stopY) leapY = stopY;
+                    if (leapY < stopY) {
+                      leapY = stopY;
+                    }
                     timer++;
                 }
             };
--- src/app/shared/ng2-backtop.module.ts
+++ src/app/shared/ng2-backtop.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {BacktopDirective} from './ng2-backtop.directive'
+import {BacktopDirective} from './ng2-backtop.directive';
 
 
 @NgModule({
